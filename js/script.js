e63b69ba5afff1d1973e873ccab475664b3f243f
Omar Ramón Montes <omarmontes.879@gmail.com>
2019-05-05 19:52:11 -0500

    Template terminado

--- js/script.js
+++ js/script.js
@@ -6,4 +6,20 @@ document.addEventListener('DOMContentLoaded', function () {
     // parallax
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, {});
+
+    // media 
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, {});
+
+    // tabs 
+    var elems = document.querySelector('#tab'); 
+    var instance = M.Tabs.init(elems, {});
+
+    // dropdown
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, {alignment: 'right', coverTrigger: false, constrainWidth: false});
+
+    // modal 
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
 });
